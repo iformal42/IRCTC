63ffe94e84064d23683a336c661500341994294a
musaif bangi <bangimusaif2002@gmail.com>
2026-09-14 20:28:18 +0530

    prisma setup donw

--- user-service/src/config/prisma.js
+++ user-service/src/config/prisma.js
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import config from "./index.js";
 const globalForPrisma = global;
-const prisma = globalForPrisma.prisma || new PrismaClient({});
-
-if (config.NODE_ENV !== "production") {
+const connctionString = config.DATABASE_URL;
+if (!globalForPrisma.prisma) {
+  const adapter = new PrismaPg({ datasourceUrl: connctionString });
+  const prisma = new PrismaClient({
+    adapter,
+    log: ["error", "warn", "info"],
+  });
   globalForPrisma.prisma = prisma;
 }
-
-export default prisma;
