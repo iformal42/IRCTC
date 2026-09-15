f602a3258d51036c34c69b825f2975716a88c1c4
musaif bangi <bangimusaif2002@gmail.com>
2026-09-16 00:32:04 +0530

    req middleware fixed

--- user-service/src/middlewares/req.middleware.js
+++ user-service/src/middlewares/req.middleware.js
@@ -1,10 +1,11 @@
 import logger from "../config/logger.js";
 
 const reqLogger = (req, res, next) => {
+  // console.log("req.method");
   logger.debug(`[${req.method}] [${req.originalUrl}] `);
   const start = Date.now();
 
-  req.on("finish", () => {
+  res.on("finish", () => {
     const duration = Date.now() - start;
     logger.info(
       `[${req.method}] [${req.originalUrl}] - status: [${res.statusCode}] [${duration}ms]`,
