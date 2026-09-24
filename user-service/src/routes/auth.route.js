f9feba26bc52a9a40e9089eae3187c487ef625c1
musaif bangi <bangimusaif2002@gmail.com>
2026-09-25 00:45:15 +0530

    google auth route implemented

--- user-service/src/routes/auth.route.js
+++ user-service/src/routes/auth.route.js
@@ -3,6 +3,7 @@ import {
   login,
   rotatedRefreshToken,
   sentOTP,
+  verifyGoogleIdToken,
   verifyOtp,
 } from "../controllers/auth.controller.js";
 const router = express.Router();
@@ -10,6 +11,7 @@ const router = express.Router();
 router.post("/sent-otp", sentOTP);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
+router.post("/google-auth", verifyGoogleIdToken);
 router.get("/refresh", rotatedRefreshToken);
 
 export default router;
